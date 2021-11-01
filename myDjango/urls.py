37cad6d8f4565dca8d3b89489e806ba6e95047fa
Hoesung Jung <jrps2212@gmail.com>
2021-11-01 15:53:59 +0900

    생성 삭제와 댓글 기능 구현 완료

--- myDjango/urls.py
+++ myDjango/urls.py
@@ -1,7 +1,7 @@
-"""myDjango URL Configuration
+"""pybo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-from board import views
+from pybo import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-    path('board/', include('board.urls')),
+    path('pybo/', include('pybo.urls')),
+    path('', views.index, name='index'),  # '/' 에 해당되는 path
 ]
---
+++ pybo/views.py
@@ -0,0 +1,120 @@
+from django.http import HttpResponse  # 삭제
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question, Answer
+from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
+
+def index(request):
+    """
+    블로그 목록 출력
+    """
+    # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+
+    # 조회
+    question_list = Question.objects.order_by('-create_date')
+
+    # 페이징처리
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    하나의 글 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    """
+    댓글 등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
+
+def question_create(request):
+    """
+    게시글 등록
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
+
+def question_modify(request, question_id):
+    """
+    질문수정
+    """
+    question = get_object_or_404(Question, pk=question_id)
+
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()  # 수정일시 저장
+            question.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
+
+def question_delete(request, question_id):
+    """
+    질문삭제
+    """
+    question = get_object_or_404(Question, pk=question_id)
+
+    question.delete()
+    return redirect('pybo:index')
+
+def answer_modify(request, answer_id):
+    """
+    답변수정
+    """
+    answer = get_object_or_404(Answer, pk=answer_id)
+
+    if request.method == "POST":
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('pybo:detail', question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+    context = {'answer': answer, 'form': form}
+    return render(request, 'pybo/answer_form.html', context)
+
+def answer_delete(request, answer_id):
+    """
+    답변삭제
+    """
+    answer = get_object_or_404(Answer, pk=answer_id)
+
+    answer.delete()
+
+    return redirect('pybo:detail', question_id=answer.question.id)
