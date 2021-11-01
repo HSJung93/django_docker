010d9739be92bb2a8cfacb1c25308d1db05de865
Hoesung Jung <jrps2212@gmail.com>
2021-11-01 15:55:57 +0900

    url board로 변경

--- myDjango/urls.py
+++ myDjango/urls.py
@@ -19,6 +19,6 @@ from pybo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pybo/', include('pybo.urls')),
+    path('board/', include('pybo.urls')),
     path('', views.index, name='index'),  # '/' 에 해당되는 path
 ]
